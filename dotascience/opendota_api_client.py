97d7e71b11d74844b4a094330ee47ad6f9aecf1f
CowardlyBattleCat <thomas.harlan.adams@gmail.com>
2018-11-28 15:20:26 -0800

    Add dotascience/transformers.py with working function to pull match-ids from pro match stubs. Modify dotascience/open_dota_api_client.py with working functions get_pro_match_stubs and get_bulk_match_data. Modify DotaScience_EDA.ipynb with exploratory code.

--- dotascience/opendota_api_client.py
+++ dotascience/opendota_api_client.py
@@ -13,6 +13,13 @@ def get_api_key(filename: str=default_secrets_path) -> str:
 
 api_key = get_api_key()
 
+def get_hero_data(api_key: str=api_key) -> List[dict]:
+    """Retrieve data for heroes from the OpenDota api."""
+    url = 'https://api.opendota.com/api/heroes'
+    params = {'api_key': api_key}
+    response = requests.get(url, params=params)
+    return response.json()
+
 def get_match_data(match_id: int, api_key: str=api_key) -> dict:
     """Retrieve data for specfied match from the OpenDota api."""
     url = 'https://api.opendota.com/api/matches/' + str(match_id)
@@ -20,9 +27,19 @@ def get_match_data(match_id: int, api_key: str=api_key) -> dict:
     response = requests.get(url, params=params)
     return response.json()
 
-def get_hero_data(api_key: str=api_key) -> List[dict]:
-    """Retrieve data for heroes from the OpenDota api."""
-    url = 'https://api.opendota.com/api/heroes'
-    params = {'api_key': api_key}
+def get_pro_match_stubs(less_than_match_id: int,
+                        api_key: str=api_key) -> List[dict]:
+    """Retrieve data stubs for pro matches with match ids smaller than
+    less_than_match_id from the OpenDota api.
+    """
+    url = 'https://api.opendota.com/api/proMatches'
+    params = {'api_key': api_key, 'less_than_match_id': less_than_match_id}
     response = requests.get(url, params=params)
     return response.json()
+
+def get_bulk_match_data(match_ids: List[int]) -> List[dict]:
+    bulk_match_data = []
+    for match_id in match_ids:
+        bulk_match_data.append(get_match_data(match_id))
+        print(match_id)
+    return bulk_match_data
---
+++ dotascience/transformers.py
@@ -0,0 +1,10 @@
+from typing import List, Dict, Any
+
+
+def get_match_ids(pro_match_data: List[dict]) -> List[int]:
+    """Return list of match ids from list of pro match stubs."""
+    match_ids = []
+    for match in pro_match_data:
+        if (match['patch'] >= min_patch and match['patch'] <= max_patch):
+        match_ids.append(match['match_id'])
+    return match_ids
