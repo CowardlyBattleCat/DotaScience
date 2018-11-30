40511ac99b455054429f809cb5dd3f6f0761d929
CowardlyBattleCat <thomas.harlan.adams@gmail.com>
2018-11-30 10:28:19 -0800

    Modify DotaScience_EDA.ipynb with function versions of utility tools. Add untested functions to utility.py

---
+++ dotascience/utility.py
@@ -0,0 +1,42 @@
+from typing import List, Dict, Any, Optional
+
+def save_as_csv(data: List[int], filename_path: str):
+    """Save data as a .csv file of integers using the given filename path,
+    adding .csv to end of file if needed.
+    """
+    if filename_path[-4:] != '.csv':
+        filename_path += '.csv'
+    with open(filename_path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows([data])
+
+def load_csv(filename_path: str) -> List:
+    """Load .csv file of integers as a list of integers using specfied path."""
+    with open(filename_path, newline='') as f:
+        reader = csv.reader(f)
+        test_list = []
+        for row in reader:
+            row = [int(x) for x in row]
+            test_list.extend(row)
+
+def save_as_json(data, filename_path: str):
+    """Save data as a .json file using the given filename path, adding .json
+    to end of file if needed.
+    """
+    if filename_path[-5:] != '.json':
+        filename_path += '.json'
+    with open(filename_path, 'w') as outfile:
+        json.dump(data, outfile)
+
+def load_json(filename_path: str):
+    """Load .json from specified filename path."""
+    with open(filename_path) as infile:
+        return json.load(infile)
+
+def find_index(match_list: List[int], match_id: int):
+    """Consume a list of matches and a specific match id. Return index for
+    match id.
+    """
+    for i, match in enumerate(match_list):
+        if match == match_id:
+            return (i, match)
