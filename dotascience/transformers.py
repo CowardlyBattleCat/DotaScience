7ca91629695b38fca989beaaeb27803520d472e8
CowardlyBattleCat <thomas.harlan.adams@gmail.com>
2018-12-02 20:02:48 -0800

    Modify DotaScience_EDA.ipynb to clean up code. Modify transformers.py with notebook tested functions.

--- dotascience/transformers.py
+++ dotascience/transformers.py
@@ -3,6 +3,7 @@
 from typing import List, Dict, Any, Tuple
 
 
+# Functions for use with .json objects
 def get_match_ids(pro_match_data: List[dict]) -> List[int]:
     """Consume list of pro match stubs. Return list of match ids."""
     match_ids = []
@@ -32,3 +33,38 @@ def filter_by_patch(bulk_match_data: List[Dict],
         if (match_patch >= min_patch and match_patch <= max_patch):
             filtered_matches.append(match)
     return filtered_matches
+
+
+# Functions for use with Pandas objects
+def make_unique_list(column):
+    """Return a list of unique levels present in lists from a given column."""
+    unique_levels=[]
+    for item in column:
+        if type(item)==list:
+            for level in item:
+                if level not in unique_levels:
+                    unique_levels.append(level)
+    unique_levels.sort()
+    return unique_levels
+
+def is_level_in_col(column, level):
+    """"""
+    if type(column) != list:
+        return 0
+    elif level in column:
+        return 1
+    else:
+        return 0
+
+def clean_df(df, column: str):
+    """"""
+    return df.drop(labels=column, axis=1)
+
+def make_list_dummies(df, column: str):
+    dummies_df = pd.DataFrame(df[column].copy())
+    for level in make_unique_list(dummies_df[column]):
+        dummies_df[f'{column}_{level.replace(" ", "-")}'] = dummies_df[column].apply(
+            lambda col: is_level_in_col(col, level)
+        )
+    dummies_df = clean_df(df=dummies_df, column=column)
+    return dummies_df
